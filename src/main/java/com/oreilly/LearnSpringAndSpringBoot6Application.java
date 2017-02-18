6b68112788ed317ed26076b00e934b43f4596559
fnunezkanut <fnunezkanut@users.noreply.github.com>
2017-02-18 15:52:00 +0000

    Initial commit

---
+++ src/main/java/com/oreilly/LearnSpringAndSpringBoot6Application.java
@@ -0,0 +1,12 @@
+package com.oreilly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringAndSpringBoot6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringAndSpringBoot6Application.class, args);
+	}
+}
